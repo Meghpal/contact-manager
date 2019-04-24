6e2247272f83018c6a12b2374c8c7ad7bf1eb062
Meghpal <40922889+Meghpal@users.noreply.github.com>
2019-04-24 18:45:33 +0530

    Made signup work

--- contact-manager/scripts/script.js
+++ contact-manager/scripts/script.js
@@ -1,42 +1,41 @@
-            function checkPassword(form) { 
-                console.log('as');
-                password1 = form.password1.value; 
-                password2 = form.password2.value; 
-  
-             
-                if (password1 == '') {
-                    alert ("Please enter Password"); 
-                    return false;
-                }
-               
-                else if (password2 == '') {
-                    alert ("Please enter confirm password"); 
-                    return false;
-                }
-                
-                else if (password1.length < 8){
-                    alert ("\nPassword must be 8 digits") ;
-                    return false; 
-                }
-                
-                else if (password1 != password2) { 
-                    alert ("\nPassword did not match: Please try again...") ;
-                    return false; 
-                } 
-  
-               
-                else{ 
-                    usersArray= JSON.parse(window.localStorage.getItem('users'));
-                    console.log((form.name.value, form.email.value, form.password1.value));
-                    user = new User(form.name.value, form.email.value, form.password1.value);
-                    userArray.push(user);
+function checkPassword(form) {
+    password1 = form.password1.value;
+    password2 = form.password2.value;
 
-                    localStorage.setItem('users', JSON.stringify(usersArray))
-                    alert("Password Match: Welcome to Contacts manager") ;
-                    return false; 
-                    
-                } 
-                return false;
-               
-            } 
-        
+    name = document.querySelector("#user").value;
+    email = document.querySelector("#mail").value;
+
+    if (password1 == '') {
+        alert("Please enter Password");
+        return false;
+    }
+
+    else if (password2 == '') {
+        alert("Please enter confirm password");
+        return false;
+    }
+
+    else if (password1.length < 8) {
+        alert("\nPassword must be 8 digits");
+        return false;
+    }
+
+    else if (password1 != password2) {
+        alert("\nPassword did not match: Please try again...");
+        return false;
+    }
+
+
+    else {
+        usersArray = JSON.parse(window.localStorage.getItem('users'));
+        user = new User(name, email, form.password1.value);
+        usersArray.push(user);
+
+        localStorage.setItem('users', JSON.stringify(usersArray))
+        alert("Password Match: Welcome to Contacts manager");
+        return true;
+
+    }
+    return false;
+
+}
